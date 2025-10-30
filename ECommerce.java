ebb3ddba8943e89aea71a145d8e4bbe3086d7cd5
Lucas Oliveira <luckass1088@gmail.com>
2025-10-30 00:52:23 -0300

    Remove comment in ECommerce interface

--- ECommerce.java
+++ ECommerce.java
@@ -1,7 +1,6 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-// Interface remota — define os métodos acessíveis via RPC
 public interface ECommerce extends Remote {
     boolean verificarEstoque(String produto, int quantidade) throws RemoteException;
     String realizarCompra(String produto, int quantidade) throws RemoteException;
