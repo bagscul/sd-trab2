5607490fadcb893b6334c6be6dd9d52854cf974e
Lucas Oliveira <luckass1088@gmail.com>
2025-10-30 00:00:48 -0300

    Implement NATS consumer to receive messages

---
+++ NatsConsumer.java
@@ -0,0 +1,22 @@
+import io.nats.client.Connection;
+import io.nats.client.Dispatcher;
+import io.nats.client.Nats;
+import java.io.IOException;
+
+public class NatsConsumer {
+    public static void main(String[] args) {
+        try {
+            Connection nc = Nats.connect("nats://<IP_NATS>:4222");
+            System.out.println("[NATS] Conectado e aguardando mensagens...");
+
+            Dispatcher d = nc.createDispatcher((msg) -> {
+                String conteudo = new String(msg.getData());
+                System.out.println("[NATS] Nova mensagem recebida: " + conteudo);
+            });
+
+            d.subscribe("pedidos.confirmados");
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
